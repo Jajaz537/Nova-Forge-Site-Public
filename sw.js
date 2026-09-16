b6298ad70adecc46c3ae798543da991660f8dbac
Jajaz537 <stephdu593000@gmail.com>
2026-09-17 00:31:41 +0200

    Restore cinematic depth to MODARYX profiles

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nova-site-shell-v118-community-sequence';
+const CACHE_NAME = 'nova-site-shell-v119-profile-depth';
 const BASE_URL = new URL('./', self.location.href);
 const PUBLIC_PAGE_PATHS = [
   './',
