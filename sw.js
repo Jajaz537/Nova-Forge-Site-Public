4089b199649aa17bf73ec3696f86f85d364ae0a2
Jajaz537 <stephdu593000@gmail.com>
2026-09-16 23:06:51 +0200

    Allow Smart Profile rows to wrap on narrow screens

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nova-site-shell-v110-modaryx-profile-result';
+const CACHE_NAME = 'nova-site-shell-v111-modaryx-profile-result';
 const BASE_URL = new URL('./', self.location.href);
 const PUBLIC_PAGE_PATHS = [
   './',
