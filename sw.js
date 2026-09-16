e83cf22c907145ae5af8059facda0a3f20b10cf1
Jajaz537 <stephdu593000@gmail.com>
2026-09-16 19:53:44 +0200

    Align search hero and download navigation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nova-site-shell-v103-modaryx-cinematic';
+const CACHE_NAME = 'nova-site-shell-v104-modaryx-cinematic';
 const BASE_URL = new URL('./', self.location.href);
 const PUBLIC_PAGE_PATHS = [
   './',
